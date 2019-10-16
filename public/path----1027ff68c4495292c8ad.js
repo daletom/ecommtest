ef6e9a2faa1cd05c0b542c086bc0762ccfba18c8
Tom Dale <tom@imgix.com>
2019-10-16 14:22:34 -0700

    change folder setup

---
+++ public/path----1027ff68c4495292c8ad.js
@@ -0,0 +1,2 @@
+webpackJsonp([60335399758886],{102:function(t,a){t.exports={data:{site:{siteMetadata:{siteName:"My Shop"}}},layoutContext:{}}}});
+//# sourceMappingURL=path----1027ff68c4495292c8ad.js.map
---
+++ public/styles-0b4508c6eeeaaf22f109.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[4],{203:function(n,w,o){}}]);
+//# sourceMappingURL=styles-0b4508c6eeeaaf22f109.js.map
