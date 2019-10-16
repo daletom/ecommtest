4ac89a89fe836a242cdf591b1f4027439d54d472
Tom Dale <tom@imgix.com>
2019-10-16 14:42:14 -0700

    Revert "Merge pull request #1 from matjack1/updates"
    This reverts commit 3d294f711b90f41fdddf486472cf163e643994b6, reversing
    changes made to 91395a3592f0ab9fccc52a6c4f03cb6fb3bcf932.

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -5,7 +5,7 @@ import Link from 'gatsby-link'
 
 import '../style/index.scss'
 
-const Layout = ({ children, site }) => (
+const Layout = ({ children, data }) => (
   <div>
     <Helmet title="Snipcart + DatoCMS + GatsbyJS Example" />
     <div className="Container">
@@ -13,26 +13,26 @@ const Layout = ({ children, site }) => (
         <div className="Wrap" >
           <div className="Header__body">
             <h1 className="Header__title">
-              <Link data-text={site.siteMetadata.siteName} to="/">
-                {site.siteMetadata.siteName}
+              <Link data-text={data.site.siteMetadata.siteName} to="/">
+                {data.site.siteMetadata.siteName}
               </Link>
             </h1>
-            <div className="Header__summary snipcart-summary snipcart-checkout">
+            <a href="#" className="Header__summary">
               <div className="Header__summary__title">
                 🛍 MY CART 🛍
               </div>
               <div className="Header__summary__line">
-                Number of items: <span className="snipcart-total-items"></span>
+                Number of items: <span>0</span>
               </div>
               <div className="Header__summary__line">
-                Total price: <span className="snipcart-total-price"></span>
+                Total price: <span>0</span>
               </div>
-            </div>
+            </a>
           </div>
         </div>
       </div>
       <div className="Wrap" >
-        {children}
+        {children()}
       </div>
       <div className="Wrap" >
         <div className="Footer">
@@ -50,3 +50,12 @@ Layout.propTypes = {
 
 export default Layout
 
+export const query = graphql`
+  query LayoutQuery {
+    site {
+      siteMetadata {
+        siteName
+      }
+    }
+  }
+`
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,99 +1,51 @@
-// import React from 'react'
-
-// const IndexPage = ({ data }) => (
-//   <div className="Catalogue">
-//     {
-//       data.products.nodes.map(product => (
-//         <div
-//           className="Catalogue__item"
-//           key={product.slug}
-//         >
-//           <a href="#" className="Product">
-//             <div className="Product__image">
-//               <img src={product.image.url} />
-//             </div>
-//             <div className="Product__details">
-//               <div className="Product__name">
-//                 {product.name}
-//                 <div className="Product__price">
-//                   {product.price}€
-//                 </div>
-//               </div>
-//               <span className="Product__buy">
-//                 Buy now
-//               </span>
-//             </div>
-//           </a>
-//         </div>
-//       ))
-//     }
-//   </div>
-// )
-
-
-
 import React from 'react'
-import { StaticQuery, graphql } from 'gatsby'
-import Layout from "../layouts/index"
-import Img from 'gatsby-image'
+import Link from 'gatsby-link'
 
-export default () => (
-  <StaticQuery
-    query={graphql`
-      query CatalogueQuery {
-        products: allDatoCmsProduct {
-          edges {
-            node {
-              id
-              name
-              price
-              image {
-                url
-                sizes(maxWidth: 300, imgixParams: { fm: "jpg" }) {
-                  ...GatsbyDatoCmsSizes
-                }
-              }
-            }
-          }
-        }
-        site {
-          siteMetadata {
-            siteName
-          }
-        }
-      }
-    `}
-render={data => (
-  <Layout site={data.site}>
-    <div className="Catalogue">
-      {
-        data.products.edges.map(({ node: product }) => (
-          <div className="Catalogue__item" key={product.id}>
-            <div
-              className="Product snipcart-add-item"
-              data-item-id={product.id}
-              data-item-price={product.price}
-              data-item-image={product.image.url}
-              data-item-name={product.name}
-              data-item-url={`/`}
-            >
-              <div className="Product__image">
-                <Img sizes={product.image.sizes} />
-              </div> <div className="Product__details">
-                <div className="Product__name">
-                  {product.name}
-                  <div className="Product__price">
-                    {product.price}€
-                  </div>
-                </div>
-                <span className="Product__buy">Buy now</span>
-              </div>
+const IndexPage = ({ data }) => (
+  <div className="Catalogue">
+    {
+      data.site.siteMetadata.products.map(product => (
+        <div
+          className="Catalogue__item"
+          key={product.slug}
+        >
+          <a href="#" className="Product">
+            <div className="Product__image">
+              <img src={product.image} />
             </div>
-          </div>
-        ))
-      }
-    </div>
-  </Layout>
-     )}
-   />
+            <div className="Product__details">
+              <div className="Product__name">
+                {product.name}
+                <div className="Product__price">
+                  {product.price}€
+                </div>
+              </div>
+              <span className="Product__buy">
+                Buy now
+              </span>
+            </div>
+          </a>
+        </div>
+      ))
+    }
+  </div>
 )
+
+export default IndexPage
+
+export const query = graphql`
+  query CatalogueQuery {
+    products: AllDatoCmsProduct {
+      edges {
+        node {
+          id
+          name
+          price
+          image {
+            url
+          }
+        }
+      }
+    }
+  }
+`
